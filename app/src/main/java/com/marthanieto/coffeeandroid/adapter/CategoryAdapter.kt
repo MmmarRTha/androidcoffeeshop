bd002e057eee34e4ab874ebc23a2a458576959a7
MmmarRTHa <m27842n@outlook.com>
2026-04-02 17:17:28 -0700

    - update: feature/category_in_dashboard - add new packages and classes to work with category adapter

--- app/src/main/java/com/marthanieto/coffeeandroid/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/marthanieto/coffeeandroid/adapter/CategoryAdapter.kt
@@ -1,16 +1,21 @@
 package com.marthanieto.coffeeandroid.adapter
 
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.database.core.Context
+import com.marthanieto.coffeeandroid.R
 import com.marthanieto.coffeeandroid.databinding.ViewholderCategoryBinding
 import com.marthanieto.coffeeandroid.domain.CategoryDomain
 
 class CategoryAdapter(val items: MutableList<CategoryDomain>) :
     RecyclerView.Adapter<CategoryAdapter.Viewholder>() {
     private lateinit var context: Context
-    private val selectedPosition = -1
-    private val lastSelectedPosition = -1
+    private var selectedPosition = -1
+    private var lastSelectedPosition = -1
 
 
     class Viewholder(val binding: ViewholderCategoryBinding) :
@@ -20,14 +25,36 @@ class CategoryAdapter(val items: MutableList<CategoryDomain>) :
         parent: ViewGroup,
         viewType: Int
     ): CategoryAdapter.Viewholder {
-        TODO("Not yet implemented")
+        context = parent.context
+        val binding = ViewholderCategoryBinding.inflate(
+            LayoutInflater.from(context), parent, false
+        )
+        return Viewholder(binding)
     }
 
-    override fun onBindViewHolder(p0: CategoryAdapter.Viewholder, p1: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: CategoryAdapter.Viewholder, position: Int) {
+        val item = items[position]
+
+        holder.binding.titleCat.text = item.title
+
+        holder.binding.root.setOnClickListener {
+            lastSelectedPosition = selectedPosition
+            selectedPosition = position
+            notifyItemChanged(lastSelectedPosition)
+            notifyItemChanged(selectedPosition)
+
+            Handler(Looper.getMainLooper()).postDelayed({
+
+            }, 500)
+        }
+        if (selectedPosition == position) {
+            holder.binding.titleCat.setBackgroundColor(R.drawable.brown_full_corner)
+            holder.binding.titleCat.setTextColor(ContextCompat.getColor(context, R.color.white))
+        } else {
+            holder.binding.titleCat.setBackgroundColor(R.drawable.cream_full_corner)
+            holder.binding.titleCat.setTextColor(ContextCompat.getColor(context, R.color.darkBrown))
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = items.size
 }
