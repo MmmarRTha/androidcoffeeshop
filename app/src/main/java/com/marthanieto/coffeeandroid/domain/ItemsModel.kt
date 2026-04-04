d4145a6ef512c8f522a3b68a021240a70dcac4d6
MmmarRTHa <m27842n@outlook.com>
2026-04-04 00:17:44 -0700

    - update: main - fix margin horizontal on popular item

--- app/src/main/java/com/marthanieto/coffeeandroid/domain/ItemsModel.kt
+++ app/src/main/java/com/marthanieto/coffeeandroid/domain/ItemsModel.kt
@@ -6,7 +6,7 @@ data class ItemsModel(
     var title: String = "",
     var description: String = "",
     var picUrl: ArrayList<String> = ArrayList(),
-    var price: Double = 0.00,
+    var price: Double = 0.0,
     var rating: Double = 0.0,
     var numberInCart: Int = 0,
     var extra: String = ""
