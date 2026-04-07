848a07178edfa3d1d49bc48865c9c63b26eafb5b
MmmarRTHa <m27842n@outlook.com>
2026-04-06 18:37:42 -0700

    - update: feature/list_of_items_activity - add ui list of items and activity functionality

--- app/src/main/java/com/marthanieto/coffeeandroid/repository/MainRepository.kt
+++ app/src/main/java/com/marthanieto/coffeeandroid/repository/MainRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
 import com.google.firebase.database.ValueEventListener
 import com.marthanieto.coffeeandroid.domain.BannerModel
 import com.marthanieto.coffeeandroid.domain.CategoryModel
@@ -59,6 +60,29 @@ class MainRepository {
         val listData = MutableLiveData<MutableList<ItemsModel>>()
         val ref = firebaseDatabase.getReference("Popular")
         ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val list = mutableListOf<ItemsModel>()
+                for (childSnapshot in snapshot.children) {
+                    val item = childSnapshot.getValue(ItemsModel::class.java)
+                    item?.let { list.add(it) }
+                }
+                listData.value = list
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        return listData
+    }
+
+    fun loadItemCategory(categoryId: String): LiveData<MutableList<ItemsModel>> {
+        val listData = MutableLiveData<MutableList<ItemsModel>>()
+        val ref = firebaseDatabase.getReference("Items")
+        val query: Query = ref.orderByChild("categoryId").equalTo(categoryId)
+
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val list = mutableListOf<ItemsModel>()
                 for (childSnapshot in snapshot.children) {
